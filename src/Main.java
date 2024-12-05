e6db1c8ee46a73154aab41e7d8da812df3672854
Lara <lara.oliveira@zup.com.br>
2024-12-05 16:39:23 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String nome = "Lara";
+        int idade = 26;
+        double altura = 1.70;
+        boolean matriculado = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Matriculado: " + matriculado);
     }
 }
